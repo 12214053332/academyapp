b109ad2bd79c4a441d98a8c101bf2d8c73ba312f
mostafa.elnamr <Most024466271>
2019-01-01 12:56:31 +0200

    last update 1-1-2018 add inAppPurchase

--- js/index.js
+++ js/index.js
@@ -45,19 +45,22 @@ var APIURL="https://www.e3melbusiness.com/";
 var tokenNumber="ay5t9Xh4hmAXSUEBby9j9dSAxjNCtnrFKp6x9YqG43JaXbpHESvHsP9G4vCg";
 var productId = 'com.e3melbusiness';
 var validationData = {};
-inAppPurchase
-    .buy(productId)
-    .then(function(res) {
-    validationData = res;
-    // give the user credits for their purchase
-    return inAppPurchase.consume(productId); // <- consumable products must be consumed
-})
-.then(function() {
+console.log(window);
+if ("inAppPurchase" in window) {
+    window.inAppPurchase
+        .buy(productId)
+        .then(function (res) {
+            validationData = res;
+            // give the user credits for their purchase
+            return window.inAppPurchase.consume(productId); // <- consumable products must be consumed
+        })
+        .then(function () {
 
-})
-.catch(function(err){
-    console.log(err)
-});
+        })
+        .catch(function (err) {
+            console.log(err)
+        });
+}
 url = window.location.pathname;
 var filename = url.substring(url.lastIndexOf('/')+1);
 var errorMessages={
