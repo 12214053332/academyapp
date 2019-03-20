e5e882322963c8e0223aa4e0a09433ffaf9cb4de
mostafa.elnamr <Most024466271>
2019-03-20 13:38:24 +0200

    courses inAppPurchase

--- js/courses.js
+++ js/courses.js
@@ -32,6 +32,13 @@ courses ={
                html='';
                var devicePlatform = device.platform;
                if(devicePlatform!='Android'){
+                   window.inAppPurchase.getReceipt().then(function(receipt) {
+                       // Handle receipt
+                        console.log(receipt)
+                   }).catch(function (error) {
+                       // Handle error
+                       console.log(error)
+                   });
                    html+='<div style="padding: 20px;margin: 20px;" class="text-center"><a href="subscription_courses.html" class="button btn-block text-center subscriptions-button">اشترك الأن</a></div>'
                }
                db.transaction(function(tx){
